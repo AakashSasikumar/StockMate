086929999855becf282d6a615506bd80b534ec50
Aakash Sasikumar <aakash3697@gmail.com>
2020-06-26 18:19:40 +0530

    implemented data download jobs

--- Core/Jobs.py
+++ Core/Jobs.py
@@ -1,5 +1,6 @@
 import schedule
 import sys
+sys.path.append("/Users/aakashsasikumar/Documents/Code/Python/StockMate/")
 from Utils import UIInitializer as uint
 import inspect
 import pandas as pd
@@ -10,9 +11,10 @@ import json
 from threading import Thread
 import time
 from Examples.Processors import BasicProcessors
+import datetime
 
 
-source = APIInterface.YFinance()
+uint.init()
 allJobTags = []
 stopJobs = False
 
@@ -34,13 +36,19 @@ class SubscriptionJob():
         The save location of the agent. Used for loading and inferencing
     tickerDataPath: str, optional
         The location of all the ticker data
+    source: APIInterface
+        An instance of one of the APIInterfaces. By default it is
+        YFinance()
     """
     def __init__(self, agentName, agentSaveLoc,
-                 tickerDataPath="DataStore/StockData/"):
+                 tickerDataPath="DataStore/StockData/",
+                 rotateProxy=False):
+
         self.agentName = agentName
         self.agentSaveLoc = agentSaveLoc
         self.tickerDataLoc = tickerDataPath
-
+        self.rotateProxy = rotateProxy
+        self.source = APIInterface.YFinance(autoRotate=rotateProxy)
         self.agent = self.loadAgent()
         self.interval = self.agent.dataProcessor.interval.lower()
 
@@ -141,10 +149,10 @@ class SubscriptionJob():
         """
         if self.interval == "1d":
             for ticker in self.tickers:
-                source.saveIntraDay(ticker)
+                self.source.saveIntraDay(ticker)
         else:
             for ticker in self.tickers:
-                source.saveInterDay(ticker, self.interval)
+                self.source.saveInterDay(ticker, self.interval)
 
     def scheduleJob(self):
         """Method to define the inference and messaging frequency
@@ -202,9 +210,115 @@ class SubscriptionJob():
         jobConfig["agentName"] = self.agentName
         jobConfig["agentSaveLoc"] = self.agentSaveLoc
         jobConfig["tickerDataLoc"] = self.tickerDataLoc
+        jobConfig["rotateProxy"] = self.rotateProxy
 
         fileName = "{}.subscription.json"
         fileSavePath = savePath + fileName.format(self.name)
+        with open(fileSavePath, "w+") as f:
+            json.dump(jobConfig, f)
+
+
+class DataDownloadJob():
+    """Class for defining data download jobs
+
+    This class is used for automating data download jobs.
+    All datadownload jobs are only executed after trading hours,
+    so as to save you number of API requests.
+
+    Attributes
+    ----------
+    tagName: str
+        A unique name given for the job. tagNames are of the
+        following format
+            YYYY-MM-DD HH:MM:SS
+    tickers: list
+        A list of tickers for which the data is to be downloaded
+    rotateProxy: boolean
+        A variable that determines whether to use not not use autorotate
+        in the API source
+    tickerSaveLoc: str
+        The location at which the ticker data is to be stored
+    source: APIInterface
+        An instance of one of the APIInterfaces. By default it is
+        YFinance()
+    """
+    def __init__(self, tickers, interval,
+                 tickerSaveLoc="DataStore/StockData/",
+                 rotateProxy=False,
+                 tagName=None):
+        global tickerDownloads
+
+        if not tagName:
+            self.makeUniqueTagName()
+        else:
+            self.tagName = tagName
+        self.interval = interval.lower()
+        self.tickers = tickers
+        self.rotateProxy = rotateProxy
+        self.tickerSaveLoc = tickerSaveLoc
+
+        self.source = APIInterface.YFinance(autoRotate=rotateProxy)
+
+    def makeUniqueTagName(self):
+        """Method to make a unqiue tag name for the job
+        """
+        dt = str(datetime.datetime.now())[:-8]
+        dt = dt.replace(" ", "")
+        dt = dt.replace("-", "")
+        dt = dt.replace(":", "")
+        self.tagName = dt
+
+    def task(self):
+        """Defines the tasks to be done when subscribed
+
+        The set of tasks include,
+            1. Downloading and saving the new ticker data
+        """
+        if self.interval == "1d":
+            for ticker in self.tickers:
+                self.source.saveIntraDay(ticker)
+        else:
+            for ticker in self.tickers:
+                self.source.saveIntraDay(ticker, self.interval)
+
+    def scheduleJob(self):
+        """Method to define the inference and messaging frequency
+
+        Each schedule is tagged with a unique tag name, so as to be able
+        to stop the job when necessary.
+
+        NOTE: No matter what the interval maybe, the data is only stored
+        at the end of the day (at 3:45 PM to be precise)
+        """
+        global allJobTags
+        allJobTags.append(self.tagName)
+
+        schedule.every(1).day.at("15:45").do(self.task).tag(self.tagName)
+
+    def save(self):
+        """Method to save the current subscription
+
+        This method writes all the essential parameters into a JSON
+        so it can be loaded and a new instance can be created with ease.
+        This method saves all the parameters required for initializing
+        DataDownloadJob().
+
+        NOTE: The save path for all jobs is fixed to DataStore/JobStore. This
+              done to make loading and running of all jobs easier and seamless.
+        """
+        path = "DataStore/"
+        if "JobStore" not in os.listdir(path):
+            os.mkdir(path+"JobStore/")
+        savePath = path+"JobStore/"
+        jobConfig = {}
+        jobConfig["tagName"] = self.tagName
+        jobConfig["tickers"] = self.tickers
+        jobConfig["tickerSaveLoc"] = self.tickerSaveLoc
+        jobConfig["rotateProxy"] = self.rotateProxy
+        jobConfig["interval"] = self.interval
+
+        fileName = "{}.datadownload.json"
+        fileSavePath = savePath + fileName.format(self.tagName)
         with open(fileSavePath, "w+") as f:
             json.dump(jobConfig, f)
 
@@ -229,28 +343,66 @@ def loadSubscriptionJob(filePath):
     name = jobConfig["agentName"]
     agentSaveLoc = jobConfig["agentSaveLoc"]
     tickerDataLoc = jobConfig["tickerDataLoc"]
-    job = SubscriptionJob(name, agentSaveLoc, tickerDataLoc)
+    rotateProxy = jobConfig["rotateProxy"]
+    job = SubscriptionJob(name, agentSaveLoc,
+                          tickerDataLoc,
+                          rotateProxy)
     return job
 
 
-def loadAllSubscriptions():
-    """Method to load all saved subscriptions
+def loadDataDownloadJob(filePath):
+    """Module level method to load a specific data download job
+
+    Parameters
+    ----------
+    filePath: str
+        The location of the saved data download job
+
+    Returns
+    -------
+    job: DataDownloadJob()
+        An instance of DataDownloadJob with the parameters
+        specified in the saved file.
+    """
+    with open(filePath) as f:
+        jobConfig = json.load(f)
+
+    tagName = jobConfig["tagName"]
+    tickers = jobConfig["tickers"]
+    tickerSaveLoc = jobConfig["tickerSaveLoc"]
+    rotateProxy = jobConfig["rotateProxy"]
+    interval = jobConfig["interval"]
+
+    job = DataDownloadJob(tickers, interval, tickerSaveLoc,
+                          rotateProxy, tagName)
+    return job
+
+
+def loadAllJobs():
+    """Method to load all saved jobs
     """
     global allSubscriptionJobs
+    global allDownloadJobs
+
     rootPath = "DataStore/JobStore/"
     allSubscriptionJobs = []
+    allDownloadJobs = []
+
     jobs = os.listdir(rootPath)
-    subscriptionJobs = []
+
     for job in jobs:
         if "subscription" in job:
             filePath = rootPath + job
-            subscriptionJobs.append(filePath)
-    for job in subscriptionJobs:
-        allSubscriptionJobs.append(loadSubscriptionJob(job))
+            jobObj = loadSubscriptionJob(filePath)
+            allSubscriptionJobs.append(jobObj)
+        elif "datadownload" in job:
+            filePath = rootPath + job
+            jobObj = loadDataDownloadJob(filePath)
+            allDownloadJobs.append(jobObj)
 
 
-def startAllSubscriptions():
-    """Method to start all loaded subscriptions
+def scheduleAllJobs():
+    """Method to start all loaded jobs
     """
     global stopJobs
     stopJobs = False
@@ -294,9 +446,34 @@ def deleteSubscription(name):
     os.remove(path + name + ".subscription.json")
 
 
+def deleteDataDownload(name):
+    """Method to delete a particular data download job
+
+    This method stops the job from running and deletes the
+    saved file.
+
+    Parameters
+    ----------
+    name: str
+        The name of the data dowload job
+    """
+    schedule.clear(name)
+    path = "DataStore/JobStore/"
+    os.remove(path + name + ".datadownload.json")
+
+
 def init():
     """Method to initialize all the jobs and to get them running
     """
-    loadAllSubscriptions()
-    startAllSubscriptions()
+    loadAllJobs()
+    scheduleAllJobs()
     Thread(target=startAllJobs).start()
+
+
+if __name__ == "__main__":
+    a = SubscriptionJob("basicAgent", "DataStore/SavedModels/BasicDQN/basicAgent")
+    a.scheduleJob()
+    b = DataDownloadJob(["IOC", "INDUSINDBK"], "1d")
+    b.scheduleJob()
+    print(b.tagName)
+    b.save()
